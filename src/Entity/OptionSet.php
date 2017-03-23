e6c44f7cfaa2095cf196dcd53dc2ab186795409e
Pavel Selitskas <p.selitskas@gmail.com>
2017-03-23 12:44:44 +0300

    Code de-duplication in Entity/OptionSet

--- src/Entity/OptionSet.php
+++ src/Entity/OptionSet.php
@@ -17,6 +17,11 @@
 
 namespace AlexaCRM\CRMToolkit\Entity;
 
+/**
+ * Represents an OptionSet from CRM.
+ *
+ * @package AlexaCRM\CRMToolkit\Entity
+ */
 class OptionSet {
 
     public $name;
@@ -31,6 +36,11 @@ class OptionSet {
 
     public $isGlobal;
 
+    /**
+     * OptionSet constructor.
+     *
+     * @param \SimpleXMLElement $optionSetNode
+     */
     public function __construct( $optionSetNode ) {
         /* Determine the Name of the OptionSet */
         $this->name = (string) $optionSetNode->Name;
@@ -45,56 +55,26 @@ class OptionSet {
         $this->displayName = (string) $optionSetNode->Description->UserLocalizedLabel->Label;
 
         /* Array to store the Options for this OptionSet */
-        $optionSetValues = Array();
+        $optionSetValues = [];
 
         switch ( $this->type ) {
             case 'Boolean':
-                /* Parse the FalseOption */
-                $value                     = (int) $optionSetNode->FalseOption->Value;
-                $label                     = (String) $optionSetNode->FalseOption->Label->UserLocalizedLabel->Label[0];
-                $optionSetValues[ $value ] = $label;
-                /* Parse the TrueOption */
-                $value                     = (int) $optionSetNode->TrueOption->Value;
-                $label                     = (String) $optionSetNode->TrueOption->Label->UserLocalizedLabel->Label[0];
-                $optionSetValues[ $value ] = $label;
+                $optionSetValues = array_merge( $optionSetValues, [
+                    (int) $optionSetNode->FalseOption->Value => (string) $optionSetNode->FalseOption->Label->UserLocalizedLabel->Label[0],
+                    (int) $optionSetNode->TrueOption->Value => (string) $optionSetNode->TrueOption->Label->UserLocalizedLabel->Label[0],
+                ] );
                 break;
             case 'State':
-                foreach ( $optionSetNode->Options->OptionMetadata as $option ) {
-                    /* Parse the Option */
-                    $value = (int) $option->Value;
-                    $label = (String) $option->Label->UserLocalizedLabel->Label[0];
-                    /* Check for duplicated Values */
-                    if ( array_key_exists( $value, $optionSetValues ) ) {
-                        trigger_error( 'Option ' . $label . ' of OptionSet ' . $this->name . ' used by field ' . (String) $attribute->SchemaName . ' has the same Value as another Option in this Set', E_USER_WARNING );
-                    } else {
-                        /* Store the Option */
-                        $optionSetValues[ $value ] = $label;
-                    }
-                }
-                break;
             case 'Status':
-                foreach ( $optionSetNode->Options->OptionMetadata as $option ) {
-                    /* Parse the Option */
-                    $value = (int) $option->Value;
-                    $label = (String) $option->Label->UserLocalizedLabel->Label[0];
-                    /* Check for duplicated Values */
-                    if ( array_key_exists( $value, $optionSetValues ) ) {
-                        trigger_error( 'Option ' . $label . ' of OptionSet ' . $this->name . ' used by field ' . (String) $attribute->SchemaName . ' has the same Value as another Option in this Set', E_USER_WARNING );
-                    } else {
-                        /* Store the Option */
-                        $optionSetValues[ $value ] = $label;
-                    }
-                }
-                break;
             case 'Picklist':
                 /* Loop through the available Options */
                 foreach ( $optionSetNode->Options->OptionMetadata as $option ) {
                     /* Parse the Option */
                     $value = (int) $option->Value;
-                    $label = (String) $option->Label->UserLocalizedLabel->Label[0];
+                    $label = (string) $option->Label->UserLocalizedLabel->Label[0];
                     /* Check for duplicated Values */
                     if ( array_key_exists( $value, $optionSetValues ) ) {
-                        trigger_error( 'Option ' . $label . ' of OptionSet ' . $this->name . ' used by field ' . (String) $attribute->SchemaName . ' has the same Value as another Option in this Set', E_USER_WARNING );
+                        trigger_error( 'Option ' . $label . ' of OptionSet ' . $this->name . ' has the same Value as another Option in this Set', E_USER_WARNING );
                     } else {
                         /* Store the Option */
                         $optionSetValues[ $value ] = $label;
@@ -102,9 +82,8 @@ class OptionSet {
                 }
                 break;
             default:
-                //echo "DEFAULTOPTIONSET";
                 /* If we're using Default, Warn user that the OptionSet handling is not defined */
-                trigger_error( 'No OptionSet handling implemented for Type ' . $this->type . ' used by field ' . (String) $attribute->SchemaName . ' in Entity ' . $this->entityLogicalName, E_USER_WARNING );
+                trigger_error( 'No OptionSet handling implemented for Type ' . $this->type, E_USER_WARNING );
         }
 
         $this->options = $optionSetValues;
